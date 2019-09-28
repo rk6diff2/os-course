0378d3234e46f53e52e1f6a43fdd9978d33e7164
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-09-28 11:47:02 +0300

    changed error handeling, fixed code style to more readable, fixed asserts

--- src/utils.c
+++ src/utils.c
@@ -12,10 +12,12 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 const int RWXR_XR_X = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH; // 755
 
 status check_or_create_dir_(char *path) {
+  assert(path != NULL);
   struct stat file_stat;
   int ret_val = stat(path, &file_stat);
   if (ret_val || !S_ISDIR(file_stat.st_mode)) {
@@ -28,15 +30,15 @@ status check_or_create_dir_(char *path) {
 
 status check_or_create_path(char *path) {
   assert(path != NULL);
-  char *path_copy = path;
-  char delimiter = '/';
-  char *token = NULL;
   char *cur_path = (char *)malloc(strlen(path));
   if (!cur_path) {
     return ERROR;
   }
 
-  while ((token = strchr(path_copy, delimiter)) != NULL) {
+  const char delimiter = '/';
+  char *path_copy = path;
+  char *token = strchr(path_copy, delimiter);
+  while (token != NULL) {
     if (strlen(token)) {
       strncpy(cur_path, path, token - path);
       cur_path[token - path] = '\0';
@@ -45,6 +47,7 @@ status check_or_create_path(char *path) {
         return ERROR;
       }
       path_copy = ++token;
+      token = strchr(path_copy, delimiter);
     }
   }
 
@@ -58,8 +61,7 @@ status check_or_create_path(char *path) {
 }
 
 status copy_file(const char *path_to, const char *file) {
-  assert(path_to != NULL);
-
+  assert(path_to != NULL && file != NULL);
   struct stat file_stat = {};
   int ret_val = stat(file, &file_stat);
   if (!ret_val && S_ISREG(file_stat.st_mode)) {
@@ -67,16 +69,13 @@ status copy_file(const char *path_to, const char *file) {
     if (fd_in < 0) {
       return ERROR;
     }
-    if (fstat(fd_in, &file_stat) < 0) {
-      close(fd_in);
-      return ERROR;
-    }
+
     char *new_path = (char *)malloc(strlen(path_to) + strlen(file) + 2);
     if (!new_path) {
       return ERROR;
     }
-
     sprintf(new_path, "%s/%s", path_to, file);
+
     int fd_out = open(new_path, O_CREAT | O_RDWR | O_TRUNC, 0644);
     if (fd_out < 0) {
       close(fd_in);
@@ -84,47 +83,44 @@ status copy_file(const char *path_to, const char *file) {
       return ERROR;
     }
 
+    bool error = false;
     if (lseek(fd_out, file_stat.st_size - 1, SEEK_SET) < 0) {
-      close(fd_in);
-      close(fd_out);
-      free(new_path);
-      return ERROR;
+      error = true;
+      goto err_handler;
     }
 
     if (write(fd_out, "", 1) != 1) {
-      close(fd_in);
-      close(fd_out);
-      free(new_path);
-      return ERROR;
+      error = true;
+      goto err_handler;
     }
 
     void *src = mmap(0, file_stat.st_size, PROT_READ, MAP_SHARED, fd_in, 0);
     if (src == MAP_FAILED) {
-      close(fd_in);
-      close(fd_out);
-      free(new_path);
-      return ERROR;
+      error = true;
+      goto err_handler;
     }
 
     void *dst = mmap(0, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED,
                      fd_out, 0);
     if (dst == MAP_FAILED) {
-      close(fd_in);
-      close(fd_out);
-      free(new_path);
-      return ERROR;
+      error = true;
+      goto err_handler;
     }
 
     memcpy(dst, src, file_stat.st_size);
+
+  err_handler:
     close(fd_in);
     close(fd_out);
     free(new_path);
-    return OK;
+    return error ? ERROR : OK;
   }
   return IS_DIR;
 }
 
 status copy_files(char *path_from, char *path_to) {
+  assert(path_from != NULL && path_to != NULL);
+
   int access_status = access(path_to, R_OK);
   if (access_status < 0) {
     perror("Directory is not readable (access denied)\n");
@@ -147,7 +143,8 @@ status copy_files(char *path_from, char *path_to) {
       if (dir->d_type == DT_REG) {
         access_status = access(dir->d_name, R_OK);
         if (access_status < 0) {
-          printf("File is not readable (access denied): %s\n", dir->d_name);
+          fprintf(stderr, "File is not readable (access denied): %s\n",
+                  dir->d_name);
           //          return PERMISSION_DENIED;
         }
 
@@ -156,7 +153,8 @@ status copy_files(char *path_from, char *path_to) {
           return result;
         }
       }
-    } while ((dir = readdir(dir_from)) != NULL);
+      dir = readdir(dir_from);
+    } while (dir != NULL);
   }
   return OK;
 }
